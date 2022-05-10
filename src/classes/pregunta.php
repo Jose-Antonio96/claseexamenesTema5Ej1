75c10f3bf5b8dafcb80ed3d55385a91d322f88fe
Jose Antonio <joseapaez96@gmail.com>
2022-05-10 12:06:51 +0200

    Clase pregunta hecha

---
+++ src/classes/pregunta.php
@@ -0,0 +1,43 @@
+<?php
+namespace ITEC\PRESENCIAL\DAW\PROG\examen;
+
+class pregunta{
+    private int $uniqueidentifier;
+    private string $description;
+    private int $maxgrade;
+
+    public function __construct(int $uniqueidentifier, string $description, int $maxgrade){
+        $this->uniqueidentifier = $uniqueidentifier;
+        $this->description = $description ;
+        $this->maxgrade = $maxgrade;
+    }
+   
+    public static function createPregunta(int $uniqueidentifier, string $description, int $maxgrade){
+        return new pregunta($uniqueidentifier, $description, $maxgrade);
+    }
+
+    public function getuniqueIdentifier():int{
+        return $this->uniqueidentifier;
+    }
+
+    public function getDescription():string{
+        return $this->description;
+    }
+
+    public function getmaxGrade():int{
+        return $this->maxgrade;
+    }
+
+    public function isGradelegit(int $grade):bool{
+        return $grade <= $this->maxgrade && $grade >= 0;
+    }
+
+    public function isThisIdentifier(int $identifier):bool{
+        return $this->identifier === $identifier;
+    }
+
+    
+}
+
+
+ 
