ad0733a2f47cfd3a53af58c3164d12e4b0c98a07
Jose Antonio <joseapaez96@gmail.com>
2022-05-10 12:45:53 +0200

    Listadopreguntas hecho

---
+++ src/classes/listadopreguntas.php
@@ -0,0 +1,40 @@
+<?php 
+namespace ITEC\PRESENCIAL\DAW\PROG\examen;
+use ITEC\PRESENCIAL\DAW\PROG\examen\pregunta;
+
+class listadopreguntas{
+    private array $preguntas;
+    private int $lastuniqueidentifier;
+    /**
+     * Undocumented function
+     * 
+     * @param array $preguntas
+     * [
+     *  "identifier"=>[
+     *      "description"=>"",
+     *      "maxgrade=>0
+     *  ]
+     * ]
+     */
+    private function __construct(array $preguntas){
+        foreach($preguntas as $uniqueidentifier=>$pregunta){
+            $this->preguntas[]=pregunta::createPregunta($pregunta["description"],$uniqueidentifier,$pregunta["maxgrade"]);
+        }
+    }
+
+    public function addPregunta(pregunta $pregunta){
+        $this->preguntas[] = $pregunta;
+    }
+
+    public function getNumpreguntas():int{
+        return count($this->preguntas);
+    }
+
+    public function getTotalGrade():int{
+        $total=0;
+        foreach($this->preguntas as $pregunta){
+            $total += $pregunta->getmaxGrade();
+        }
+        return $total;
+    }
+}
