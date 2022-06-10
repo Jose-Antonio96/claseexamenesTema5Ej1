30654d8e6a6d1fce7ba2d10975cbf4917a59fbf1
José Antonio <joseapaez96@gmail.com>
2022-06-10 18:43:19 +0200

    arreglos

--- runTest.ps1
+++
@@ -1,3 +0,0 @@
-$date=$(Get-Date).ToString("yyyyMMddhhmmss")
-$OutputFileName=$date+"_ReportSuite.html"
-./vendor/bin/phpunit ./tests/examenTest.php --testdox-html ./logs/tests/$OutputFileName --coverage-html ./logs/coverage
